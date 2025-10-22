c712cbaff891d18a262efa60368bcb43016e375c
Dominik Nuszpl <dn@dinotax.de>
2025-10-22 12:11:27 +0200

    repro

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { createLoader, parseAsInteger, useQueryStates } from "nuqs";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { useLoaderData } from "react-router";
+import { useTransition } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +10,38 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const homeSearchParams = {
+  counter: parseAsInteger.withDefault(0).withOptions({ shallow: false }),
+};
+
+const loadSearchParams = createLoader(homeSearchParams);
+
+export const loader = async (args: Route.LoaderArgs) => {
+  const { counter } = loadSearchParams(args.request.url);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return { counter };
+};
+
 export default function Home() {
-  return <Welcome />;
+  const { counter: serverCounter } = useLoaderData();
+  const [isLoading, startTransition] = useTransition();
+  const [{ counter }, setSearchParams] = useQueryStates(homeSearchParams, {
+    shallow: false,
+    startTransition,
+  });
+  console.log({ isLoading, counter, serverCounter });
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>Counter: {counter}</div>
+      <div>Server Counter: {serverCounter}</div>
+      {isLoading && <div>Loading...</div>}
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={() => setSearchParams({ counter: counter + 1 })}
+      >
+        Increment
+      </button>
+    </div>
+  );
 }
